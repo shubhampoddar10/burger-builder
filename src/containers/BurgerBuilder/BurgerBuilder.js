47294cad91b1a5696a99f6663128f924c155f4ff
shubhampoddar10 <poddarshubham.sps@gmail.com>
2020-11-12 20:17:48 +0530

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliary/Auxiliary'
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+
+const INGREDIENT_PRICE = {
+    salad: 0.5,
+    bacon: 0.7,
+    cheese: 0.8,
+    meat: 1.3
+}
+
+class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+
+    //     }
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4,
+        purchasable: false,
+        purchasing: false
+    }
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey]
+        }).reduce((sum, el) => {
+            return sum + el
+        }, 0);
+        this.setState({ purchasable: sum > 0 })
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updatedCount = oldCount + 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount
+
+        const priceAddition = INGREDIENT_PRICE[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({
+            totalPrice: newPrice, ingredients: updatedIngredients
+        });
+        this.updatePurchaseState(updatedIngredients);
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount
+
+        const priceAddition = INGREDIENT_PRICE[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice - priceAddition
+        this.setState({
+            totalPrice: newPrice, ingredients: updatedIngredients
+        })
+        this.updatePurchaseState(updatedIngredients);
+
+    }
+
+    purchaseHandler = () => {
+        this.setState({ purchasing: true });
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!')
+    }
+
+    render() {
+        const disableInfo = {
+            ...this.state.ingredients
+        }
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0
+        }
+        // salad: true , meat: false,.......
+        return (
+            <Aux>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary
+                        price={this.state.totalPrice}
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
+                </Modal>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disableInfo}
+                    purchasable={this.state.purchasable}
+                    price={this.state.totalPrice}
+                    ordered={this.purchaseHandler}
+                />
+            </Aux>
+        );
+    }
+
+}
+
+export default BurgerBuilder;
