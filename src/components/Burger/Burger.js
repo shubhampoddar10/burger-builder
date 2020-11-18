6bd1fd80c32806d2603f5387d649c9c45e24a4f4
shubhampoddar10 <poddarshubham.sps@gmail.com>
2020-11-18 13:42:23 +0530

    Added Routing & orders output

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,8 +4,8 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    let transformIngredients = Object.keys(props.ingredients).map(
-        igKey => {
+    let transformIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
             return [...Array(props.ingredients[igKey])].map(
                 (_, i) => {
                     return <BurgerIngredient key={igKey + i} type={igKey} />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -96,33 +96,17 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({
-            loading: true
-        })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Shubham Poddar',
-                address: {
-                    street: 'station road',
-                    zipCode: 852101,
-                    country: 'India'
-                },
-                deliveryMethod: 'fastest'
-            }
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' +encodeURIComponent(this.state.ingredients[i]))
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({
-                    loading: false, purchasing: false
-                })
-            })
-            .catch(error => {
-                this.setState({
-                    loading: false, purchasing: false
-                })
-            })
+        queryParams.push('price=' + this.state.totalPrice )
+        const queryString = queryParams.join('&')
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
     }
 
     render() {
