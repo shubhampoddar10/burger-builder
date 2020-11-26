079495e5b7fe40a51fc61754d47b0a72bb607917
niranjan-ab-inbev <niranjan.kumar@ab-inbev.com>
2020-11-27 03:00:11 +0530

    added firebase deployment

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,11 +4,6 @@ import Aux from '../../../hoc/Auxiliary/Auxiliary'
 import Button from '../../UI/Button/Button'
 
 class OrderSummary extends Component {
-    // This could be a functional component doesn't have to be a class
-    componentDidUpdate () {
-        console.log('[OrdereSummar] Will Update');
-    }
-
     render() {
         const ingredientSummary = Object.keys(this.props.ingredients).map(
             igKey => {
---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "../NavigationItem/NavigationItem";
+
+configure({ adapter: new Adapter() });
+
+describe("<NavigationItems />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
+  it("should render two <NavigationItem /> elements if not authentiacted", () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render three <NavigationItem /> elements if authentiacted", () => {
+    //   wrapper =  shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it("should an exact logout button", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
+});
